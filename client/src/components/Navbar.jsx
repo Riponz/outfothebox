7d88273abab689233af4b8f643026e6b4df87dba
Riponz <riponbiswasfilms@gmail.com>
2023-10-10 17:01:28 +0530

    initial commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Navbar.css"
+
+function Navbar() {
+  return (
+    <div>
+        <nav className='nav'>
+            <span className="logo">outꟻothe📦</span>
+            <div className="menu">
+                <span className="menuItems">Home</span>
+                <span className="menuItems">View</span>
+                <span className="menuItems">Account</span>
+                <span className="menuItems">About Us</span>
+            </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar

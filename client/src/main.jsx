48ca9e84e9d65482b9617c69ad77cac8832e0852
Diganta Biswas <51294318+Riponz@users.noreply.github.com>
2023-10-30 23:44:07 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
+import { browserHistory } from 'react-router'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+<Router history={browserHistory} routes={routes} />,
   <React.StrictMode>
     <BrowserRouter>
     <App />

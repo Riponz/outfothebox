2052de8a876d97c5f130e433693cb326bf77bb33
Diganta Biswas <51294318+Riponz@users.noreply.github.com>
2023-10-31 00:02:37 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, browserHistory } from "react-router-dom";
-import { browserHistory } from 'react-router'
 import App from './App.jsx'
 import './index.css'
 
